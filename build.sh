7faebe075413c41b1e1d3314749bf358c7914970
Anas Al-Selwi <s316818@stud.hioa.no>
2017-02-12 20:05:14 +0000

    Unit test removed

--- build.sh
+++ build.sh
@@ -12,20 +12,9 @@ echo VERSION=$(cat version.txt) > props.env
 # Build the docker image for the application
 sudo docker build --no-cache -t ${DOCKER_USERNAME}/http-app:snapshot .
 imageid=$(sudo docker images | grep ${DOCKER_USERNAME}/http-app | grep snapshot | awk '{print $3}')
-echo "Building docker image is over"
-
-# Run the unit tests
-#sudo docker run --rm -v "$PWD":/usr/src/myapp -w /usr/src/myapp golang:1.6 go test -v
-
-# Check if a testing-app is running - if so, remove it
-#cid=$(sudo docker ps --filter="name=testing-app" -q -a)
-#if [ ! -z "$cid" ]
-#then
-#    sudo docker rm -f testing-app
-#fi
 
 # Run a container from the image and capture the container ID
-cid=$(sudo docker run -d --name wstest -p $hostport:80 ${DOCKER_USERNAME}/http-app:snapshot)
+cid=$(sudo docker run -d --name websrv-test -p $hostport:80 ${DOCKER_USERNAME}/http-app:snapshot)
 echo "cid=$cid" >> props.env
 echo "IMAGEID=$imageid" >> props.env
 cat props.env
