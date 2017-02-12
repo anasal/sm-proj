2341cd196441cf299b492c62329f51ccdb86757e
Anas Al-Selwi <s316818@stud.hioa.no>
2017-02-12 16:03:30 +0000

    added Dockerfile & build.sh files

---
+++ Dockerfile
@@ -0,0 +1,12 @@
+FROM ubuntu:16.04
+MAINTAINER Anas Al-Selwi "s316818@stud.hioa.no"
+RUN apt-get update
+RUN apt-get install -y apache2
+RUN apt-get -y install libapache2-mod-php5 php5-mysql
+ENV APACHE_RUN_USER www-data
+ENV APACHE_RUN_GROUP www-data
+ENV APACHE_LOG_DIR /var/log/apache2
+ONBUILD ADD . /var/www/html/
+EXPOSE 80
+ENTRYPOINT ["/usr/sbin/apache2ctl"]
+CMD [ "-D","FOREGROUND"]
---
+++ build.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+# Build script for webserver
+
+#Fail on non-zero
+set -e
+
+# Host port mapping for testing-app
+hostport=8000
+
+echo VERSION=$(cat version.txt) > props.env
+
+# Build the docker image for the application
+sudo docker build --no-cache -t ${DOCKER_USERNAME}/http-app:snapshot .
+imageid=$(sudo docker images | grep ${DOCKER_USERNAME}/http-app | grep snapshot | awk '{print $3}')
+echo "Building docker image is over"
+
+# Run the unit tests
+#sudo docker run --rm -v "$PWD":/usr/src/myapp -w /usr/src/myapp golang:1.6 go test -v
+
+# Check if a testing-app is running - if so, remove it
+#cid=$(sudo docker ps --filter="name=testing-app" -q -a)
+#if [ ! -z "$cid" ]
+#then
+#    sudo docker rm -f testing-app
+#fi
+
+# Run a container from the image and capture the container ID
+cid=$(sudo docker run -d --name testing-app -p $hostport:80 ${DOCKER_USERNAME}/http-app:snapshot)
+echo "cid=$cid" >> props.env
+echo "IMAGEID=$imageid" >> props.env
+cat props.env
+
+# Get the IP address of the container
+cip=$(sudo docker inspect --format '{{ .NetworkSettings.IPAddress }}' ${cid})
+
+# Check the service is reachable, using seige engine
+sudo docker run --rm rufus/siege-engine -g http://$cip:80/
+[ $? -ne 0 ] && exit 1
+
+# Clean up the container
+sudo docker kill ${cid}
+sudo docker rm ${cid}
