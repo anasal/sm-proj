6b56fdb1de980904cfd30f39b4754db6eab6114a
Anas Al-Selwi <s316818@stud.hioa.no>
2017-02-12 20:05:46 +0000

    Test script

---
+++ test.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+# Run this after build.sh.
+# Expects these environment variables:
+#
+#    IMAGEID - The docker image id to test
+#    DOCKER_USERNAME - The docker username for naming repositories
+#
+# Can run this as to use the file generated from build.sh:
+#
+#    env $(cat props.env | xargs) ./test.sh
+
+#Fail on non-zero
+set -e
+
+# Host port mapping for testing-app
+hostport=8010
+
+# Check if testing-app is running, if so, kill it
+cid=$(sudo docker ps --filter="name=websrv-test" -q -a)
+if [ ! -z "$cid" ]
+then
+    sudo docker rm -f testing-app
+fi
+
+# Run the container, name it testing-app
+echo Running the container, with --name=web-srv-test
+testing_cid=$(sudo docker run -d --name web-srv-test -p $hostport:80  $IMAGEID)
+echo "testing_cid=$testing_cid" >> props.env
+
+# Get the container IP address, and run siege engine on it for 60 seconds
+cip=$(sudo docker inspect --format '{{ .NetworkSettings.IPAddress }}' ${testing_cid})
+sudo docker run --rm rufus/siege-engine  -b -t60S http://$cip:$hostport/ > output 2>&1
+
+# Check service availability
+echo Checking service availability...
+avail=$(cat output | grep Availability | awk '{print $2}')
+echo $avail
+# shell uses = to compare strings, bash ==
+if [ "$avail" = "100.00" ]
+then
+    echo "Availability high enough"
+    sudo docker tag $IMAGEID ${DOCKER_USERNAME}/http-app:stable
+    exit 0
+else
+    echo "Availability too low"
+    exit 1
+fi
